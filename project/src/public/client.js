11fecf3803a7dc8c52f616c4e96d65780cfc1174
Jonas Surmann <jonassurmann@Jonass-MacBook-Pro.local>
2021-03-15 11:59:11 +0800

    Just testing with external code

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,44 +1,41 @@
 let store = {
-    user: { name: "NASA Enthusiast" },
-    //apod: '',
-    //curiosity: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
 }
 
-// add our markup to the page
+//root is the ID of the HTML element where content will go
 const root = document.getElementById('root')
 
+// roverForm is the ID os the dropdown menu to choose a rover
+const roverForm = () => {
+    //checks first if the dropdown menu is there
+    if (document.getElementById('roverDropDown')) return document.getElementById('roverDropDown')
+    else return 'root'
+}
+
+// updateStore is the only function that changes the values of the store object. It then renders the content
 const updateStore = (store, newState) => {
-    console.log("New State ", newState)
     store = Object.assign(store, newState)
     render(root, store)
 }
 
+//render puts content inside the root html element
 const render = async (root, state) => {
     root.innerHTML = App(state)
 }
 
-
 // create content
 const App = (state) => {
-    let { rovers } = state
+    let {
+        rovers
+    } = state
 
     return `
         <header></header>
         <main>
-            ${Greeting(store.user.name)}
+            <h1>Mars dashboard</h1>
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${roverSection(store.rovers.map(x => x))}
+                ${roverDropDown(DropDown)}
+                ${roverInfo(rovers)}
             </section>
         </main>
         <footer></footer>
@@ -50,73 +47,74 @@ window.addEventListener('load', () => {
     render(root, store)
 })
 
-// ------------------------------------------------------  COMPONENTS
+//listening for form changes to grab the chosen rovers api
+root.addEventListener('change', () => {
+    getRoverApi(store, roverForm().value)
+})
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
+// If there is a chosen rover in store, show the rover information, else show 'choose a rover' message
+const roverInfo = (rovers) => {
+    if (Array.isArray(rovers)) {
         return `
-            <h1>Welcome, ${name}!</h1>
+            <p>Choose a rover!</p>
         `
-    }
+    } else {
+        const latestImages = () => {
 
+            const filtered = rovers.image.photos.filter((x, y, z) => {
+                if (y > 0) return x.camera.full_name != z[y - 1].camera.full_name
+                else return x = x
+            });
+
+            const map1 = filtered.map((x, y) => {
+                return (`
+                    <img src='${x.img_src}' max-width:100%; height:auto />
+                    <figcaption>Taken with the ${x.camera.full_name}</figcaption>
+                `)
+            });
+
+            return (map1)
+        }
+        return (`
+        <h1>${rovers.image.photos[0].rover.name} rover</h1>
+        <p>Launch date: ${rovers.image.photos[0].rover.launch_date}</p>
+        <p>Landing date: ${rovers.image.photos[0].rover.landing_date}</p>
+        <p>Mission status: ${rovers.image.photos[0].rover.status}</p>
+        <p>Date of latest pictures: ${rovers.image.photos[0].earth_date}</p>
+        ${latestImages()}
+
+    `)
+    }
+}
+
+// the DropDown function allows us to create a custom dropdown menu
+const DropDown = (option1, option2, option3, option4, buttonId, prompt) => {
     return `
-        <h1>Hello!</h1>
+        <select id='${buttonId}'>
+        <option disabled selected value> -- ${prompt} -- </option>
+        <option>${option4}</option>
+        <option>${option1}</option>
+        <option>${option2}</option>
+        <option>${option3}</option>
+        </select>
     `
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const roverSection = (rover) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    console.log(rover);
-    //getRoverAPI(store, rover[0])
-    if (!rover || rover.date === '' ) {
-        console.log(rover);
-        getRoverAPI(store, rover)
-    } else{
-      const photodate = new Date(rover.date)
-      //console.log(store, curiosity);
-      //console.log(photodate.getDate(), today.getDate());
-      //console.log(photodate.getDate() === today.getDate());
-    }
-
-    // check if the photo of the day is actually type video!
-    if (rover.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${rover.url}">here</a></p>
-            <p>${rover.title}</p>
-            <p>${rover.explanation}</p>
-        `)
-    } else {
-        console.log(rover.image.photos[1])
-        return (`
-            <img src="${rover.image.photos[0].img_src}" height="350px" width="100%" />
-            <p>Rover: ${rover.image.photos[0].rover.name}</p>
-            <p>Launch date: ${rover.image.photos[0].rover.launch_date}</p>
-            <p>Landing date: ${rover.image.photos[0].rover.landing_date}</p>
-            <p>Status: ${rover.image.photos[0].rover.status}</p>
-            <p>Date of photo taken: ${rover.image.photos[0].earth_date}</p>
-        `)
-    }
+// the roverDropDown function is a higher order function that returns a custom dropdown using the DropDown function
+const roverDropDown = (callback) => {
+    return callback('curiosity', 'opportunity', 'spirit', 'perseverance', 'roverDropDown', 'choose a rover')
 }
 
-// ------------------------------------------------------  API CALLS
-const getRoverAPI = (state, rover) => {
-    let { rovers } = state
-
+//api call
+const getRoverApi = (state, rover) => {
+    let {
+        rovers
+    } = state
     fetch(`http://localhost:3000/${rover}`)
         .then(res => res.json())
-        .then(curiosity => updateStore(store, { rovers }))
+        .then(rovers => updateStore(store, {
+            rovers
+        }))
+
+    return this.data
 }
-
-
-// Example API call
-//const getImageOfTheDay = (state) => {
-    //let { apod } = state
-
-    //fetch(`http://localhost:3000/apod`)
-        //.then(res => res.json())
-        //.then(apod => updateStore(store, { apod }))
-//}
